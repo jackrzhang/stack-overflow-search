8b6215bbce6d5b994c324ddc6ef55f1f4583b975
Jack Zhang <15jzhang@gmail.com>
2016-08-10 16:28:30 -0400

    Adjust log results coloring

--- src/helpers/logResults.js
+++ src/helpers/logResults.js
@@ -1,19 +1,20 @@
 /* eslint-disable camelcase */
-import chalk from 'chalk';
+import { white, green, blue, yellow, magenta } from 'chalk';
 
 function logResults(results, numResults) {
-  if (results.length === 0) console.log(chalk.blue('No results found.'));
+  if (results.length === 0) console.log(blue('No results found.'));
 
   const displayedResults = results.slice(0, numResults - 1);
   displayedResults.forEach(result => {
     const { title, score, view_count, creation_date, last_edit_date, link } = result;
+    const active = last_edit_date === undefined ? 'Over a year ago' : last_edit_date;
 
-    console.log(chalk.cyan.bold(`Question: ${title}`));
-    console.log(chalk.magenta(`Asked: ${creation_date}`));
-    console.log(chalk.yellow(`Viewed: ${view_count} times`));
-    console.log(chalk.blue(`Active: ${last_edit_date}`));
-    console.log(chalk.green.bold(`Score: ${score}\n`));
-    console.log(`URL: ${link}\n`);
+    console.log(white.bold.underline(`\nQuestion: ${title}\n`));
+    console.log(white(`    Score: ${green.bold(score)}\n`));
+    console.log(white(`    Asked: ${blue.bold(creation_date)}`));
+    console.log(white(`    Viewed: ${magenta.bold(`${view_count} times`)}`));
+    console.log(white(`    Active: ${blue.bold(active)}\n`));
+    console.log(white(`    URL: ${yellow(link)}\n`));
   });
 }
 
