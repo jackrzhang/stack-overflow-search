16766d9491be4d167d86277c2563838d15e3d05f
Jack Zhang <15jzhang@gmail.com>
2016-08-09 23:07:25 -0400

    Add basic interface to sos command

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 #!/usr/bin/env node
 
-// import program from 'commander';
-// import chalk from 'chalk';
+import program from 'commander';
+import pkg from './../package.json';
+import chalk from 'chalk';
 
-console.log('Hello, world!');
+let keywords, url;
+
+program
+  .version(pkg.version)
+  .usage('[options] <keywords>')
+  .description('A simple utility for searching Stack Overflow.')
+  .parse(process.argv);
+
+if (program.args.length === 0) {
+  program.help();
+} else {
+  keywords = program.args;
+  console.log(keywords);
+}
