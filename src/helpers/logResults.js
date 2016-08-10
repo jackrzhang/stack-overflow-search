a0dbbca4e7d58e5734f80969d8b77cadc78074f0
Jack Zhang <15jzhang@gmail.com>
2016-08-10 00:18:44 -0400

    Add basic logging for search results

--- src/helpers/logResults.js
+++ src/helpers/logResults.js
@@ -1,7 +1,17 @@
+/* eslint-disable camelcase */
 import chalk from 'chalk';
 
 function logResults(results) {
-  console.log(chalk.blue(results));
+  results.forEach(result => {
+    const { title, score, view_count, creation_date, last_edit_date, link } = result;
+
+    console.log(chalk.cyan.bold.underline(`Question: ${title}`));
+    console.log(chalk.magenta(`Asked: ${creation_date}`));
+    console.log(chalk.yellow(`Viewed: ${view_count} times`));
+    console.log(chalk.blue(`Active: ${last_edit_date}`));
+    console.log(chalk.green.bold(`Score: ${score}\n`));
+    console.log(chalk.white(`URL: ${link}\n`));
+  });
 }
 
 export default logResults;
